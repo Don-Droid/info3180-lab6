e37d711ff1619f12504ce1b2f2f7e9e0bd643270
Don-Droid <raldon.baxter@mymona.uwi.edu>
2020-04-20 18:12:23 -0500

    lab 6 completed

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -7,15 +7,10 @@ Vue.component('app-header', {
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
-                  </li>
+                  <li class="nav-item active"> <router-link to="/" class="nav-link">Home</router-link> </li>
+                  <li class="nav-item active"> <router-link to="/news" class="nav-link">News</router-link>  </li>
                 </ul>
               </div>
             </nav>
@@ -26,26 +21,102 @@ Vue.component('app-header', {
     }
 });
 
-Vue.component('app-footer', {
-    template: `
-        <footer>
-            <div class="container">
-                <p>Copyright &copy {{ year }} Flask Inc.</p>
-            </div>
-        </footer>
-    `,
-    data: function() {
+const NewsList = Vue.component('news-list', {  
+      template: `
+      <div class="news">
+        <h2>News</h2>
+        <div class="form-inline d-flex justify-content-center">
+          <div class="form-group mx-sm-3 mb-2">
+            <label class="sr-only" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2"@click="searchNews">Search</button>
+          </div>
+        </div>
+        <div class="news__list">        
+          <div v-for="article in articles" class="news__item">
+            <section class= "wrapper">
+              <div id="ar">
+                {{ article.title }} 
+              </div>
+              <div id="im">
+              <br>
+                <img :src= article.urlToImage id="pic" alt="News photo">
+              </div>
+              <div id="de">
+              <br>
+                {{article.description}} 
+              </div>
+          </div>
+        </div>
+    </div>
+  `,
+      created: function() {
+        let self = this;
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=0ce5046e51434bfcb3d244bcf8c6f205').then(function(response) {
+        return response.json();
+        }).then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+        });
+      },
+      data: function() {
         return {
-            year: (new Date).getFullYear()
-        }
+          articles: [],
+          searchTerm: ''
+      }
+    },
+      methods: {
+        searchNews: function() {
+        let self = this;
+        fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=<your-api-key>').then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+    });
     }
-})
-
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
     }
 });
 
+Vue.component('app-footer', {
+  template: `
+      <footer>
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+  `,
+  data: function() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
+});
+
+const Home = Vue.component('home', {
+      template: `
+        <div class="home">
+          <img src="/static/images/logo.png" alt="VueJS Logo">
+          <h1>{{ welcome }}</h1>
+        </div>
+  `,
+  data: function() {
+  return {
+  welcome: 'Hello World! Welcome to VueJS'
+  }
+  }
+  });
+
+  const router = new VueRouter({
+    mode: 'history',
+    routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+    ]
+    });
+
+    const app = new Vue({
+      el: '#app',
+      router
+      });
